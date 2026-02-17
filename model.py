e0829b7ba3cb8d29f5c8cbfe6dc7edc4c95c4593
Francisco Ferreira da Silva <franhfsilva@gmail.com>
2026-02-17 11:13:28 +0000

    running version

--- model.py
+++ model.py
@@ -1,13 +1,17 @@
 import torch
+import pickle
+import numpy as np
 from dataclasses import dataclass
+from typing import Sequence
+from pathlib import Path
 
 @dataclass
 class Config:
 	"""Configuration class for model and training"""
 	# Model Parameters
-	n_features: int = 50
+	n_features: int = 100
 	d_embed: int = 1000
-	d_mlp: int = 100
+	d_mlp: int = 50
 
 	# Training Parameters
 	batch_size: int = 2048
@@ -23,8 +27,10 @@ class SimpleMLP():
 
 	def __init__(self, config: Config):
 		d_embed, n_features, d_mlp = config.d_embed, config.n_features, config.d_mlp
-        # The embedding vectors are random (TODO unit) vectors
-		self.W_E = torch.randn(d_embed, n_features)
+		# The embedding vectors are random vectors; not trainable
+		# Initialized with Xavier normal distribution; apparently good against vanishing gradients
+		# See https://proceedings.mlr.press/v9/glorot10a
+		self.W_E = xavier_normal((d_embed, n_features), n_features, d_embed)
 		self.W_in = torch.randn(d_mlp, d_embed)
 		self.W_in.requires_grad_()
 		self.W_out = torch.rand(d_embed, d_mlp)
@@ -33,7 +39,7 @@ class SimpleMLP():
 	@staticmethod
 	def activation(x):
 		# Just a ReLU
-		return torch.maximum(0., x)	
+		return torch.maximum(torch.zeros(1), x)	
 
 	def forward(self, x):
 		# @ is shorthand for matmul in torch starting with Python 3.5
@@ -42,7 +48,7 @@ class SimpleMLP():
 		pre_act = res @ self.W_in.T
 		post_act = self.activation(pre_act)
 		mlp_out = post_act @ self.W_out.T
-		res += mlp_out
+		res = res + mlp_out
 		# transposing twice is identity
 		return res @ self.W_E
 
@@ -55,21 +61,53 @@ class UniformFeatureDataset():
 		self.n_features = config.n_features
 
 	def generate_batch(self, batch_size):
-		return 2 * torch.rand(batch_size, n_features) - 1 
+		batch = 2 * torch.rand(batch_size, self.n_features) - 1
+		labels = SimpleMLP.activation(batch) + batch
+		return batch, labels
 
 
-def loss(output, target_output):
-	# L^2 loss
-	return torch.mean((target_output - output) ** 2)
-
-def training():
-	pass
+def xavier_normal(shape: Sequence[int], n_inputs: int, n_outputs: int) -> torch.Tensor:
+	std = np.sqrt(2. / (n_inputs + n_outputs))
+	return torch.distributions.Normal(loc=0., scale=std).sample(shape)
 
 
+def train(network: SimpleMLP, config: Config) -> SimpleMLP:	
+	optimizer = torch.optim.AdamW(params=[network.W_in, network.W_out], lr=config.lr, weight_decay=0.01)
+
+	dataset = UniformFeatureDataset(config)
+	for step in range(config.steps):
+		batch, labels = dataset.generate_batch(config.batch_size)
+		optimizer.zero_grad()  # clears gradients
+		outputs = network.forward(batch)
+		loss = torch.mean((labels - outputs) ** 2)
+		loss.backward()
+		optimizer.step()
+
+		if (step + 1) % config.print_feq == 0 or step == config.steps - 1:
+			print("Loss {} at step {}".format(loss.item(), step)) 
+
+	return network
+
+def evaluate(network: SimpleMLP, dataset: UniformFeatureDataset, batch_size: int=10000):
+	with torch.no_grad():
+		batch, labels = dataset.generate_batch(batch_size)
+		outputs = model.forward(batch)
+		loss = torch.mean((labels - outputs) ** 2).item()
+	return loss
+
+
+torch.manual_seed(42)
+np.random.seed(42)
 config = Config()
-x = torch.randn(config.n_features)
-my_mlp = SimpleMLP(config)
-my_mlp.forward(x)
+model_path = Path('model.pkl')
+if not model_path.exists():
+	model = SimpleMLP(config)
+	model = train(network=model, config=config)
+	with open(model_path, "wb") as f:
+		pickle.dump(model, f)
 
-my_features = UniformFeatureDataset()
-print(my_features.generate_batch(batch_size=config.batch_size))
+else:
+	with open(model_path, "rb") as f:
+		model = pickle.load(f)
+dataset = UniformFeatureDataset(config)
+print("Trained model loss: ", evaluate(model, dataset))
